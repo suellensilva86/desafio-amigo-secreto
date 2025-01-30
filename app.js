9aac2c507a34fe06babc25fceb64c89cb07be763
Joao Jose Rocha de Sousa <joaojoserodeso@gmail.com>
2025-01-30 17:35:00 -0300

    fixing feature adicionarAmigo

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 
 function adicionarAmigo() {
-    input = document.querySelector('input').value;
+    input = document.getElementById('amigo').value;
     if (input == '') {
         alert('Por favor, insira um nome.');
     } else {
@@ -13,22 +12,22 @@ function adicionarAmigo() {
 }
 
 function cleanCampo() {
-    document.querySelector('input').value = '';
+    document.getElementById('amigo').value = '';
 }
 
 function atualizarListaAmigos() {
-    let lista = document.querySelector('ul');
+    let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
     amigos.forEach((amigo) => {
         let item = document.createElement('li');
         item.innerHTML = amigo;
         lista.appendChild(item);
     });
-    exibirTextoNaTela(amigos, 'listaAmigos');
+    
 }
 
 function sortearAmigo() {
-    if (amigos.length != 0) {   
+    if (amigos.length > 0) {   
         let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
         document.getElementById('resultado').innerHTML = sorteado;
     }

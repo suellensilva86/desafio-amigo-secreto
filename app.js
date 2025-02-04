5475dc184e036f1bd999a7cac956dbac2620089a
Wylviane <wylviane.cruz@gmail.com>
2025-02-04 13:31:39 -0300

    alteraçoes

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let participantes = []
+
+function adicionarAmigo(){
+    let amigo = document.getElementById('amigo').value.trim();
+    if(amigo === ''){
+        alert('Por favor insira um nome antes de adicionar.');
+        return;    
+    }
+
+    if (!participantes.includes(amigo)){ //garante a não diplicidade de anigos:
+        participantes.push(amigo);//add os participantes
+        atualizarLista()
+        document.querySelector('#amigo').value = '';//limpa o campo
+    }
+}
+
+//função para atualizar a lista de participantes na interface
+function atualizarLista(){
+    let listaAmigos = document.querySelector('#listaAmigos');//obtém o elemento <ul>
+    listaAmigos.innerHTML = '';
+
+    participantes.forEach (function(participantes){
+        let item = document.createElement('li');//cria um novo elemento
+        item.textContent = participantes;
+        listaAmigos.appendChild(item);
+    });
+}
--- app_performance.js
+++ app_performance.js
@@ -18,11 +18,12 @@ function adicionarAmigo(){
 
 //função para atualizar a lista de participantes na interface
 function atualizarLista(){
-    listaAmigosEl.innerHTML = '';
+    listaAmigosEl.innerHTML = '';//limpar a lista antes de atualizá-la
 
+    //percorre a array "participantes" e add cada nome á lista
     participantes.forEach (function(participantes){
         let item = document.createElement('li');//cria um novo elemento
         item.textContent = participantes;
-        listaAmigosEl.appendChild(item);
+        listaAmigosEl.appendChild(item);//add o item criado à lista no html
     });
 }

f9cae07ba43797d284e0f01833811885ac200d89
gus <gustavohgrdev@gmaiil.com>
2025-02-11 15:20:04 -0300

    feat: implementa uma função para agregar array "amigos"

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let input_value = document.querySelector("input").value.trim();
+    const regexp = /^[a-zA-Z\s]{3,30}$/;
+
+    if (!regexp.test(input_value)) {
+        alert("Por favor, insira um nome.");
+
+    } else {
+        amigos.push(input_value);
+        input_value = "";
+    }
+}

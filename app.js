664c59ff85b136cb323af361aef72ba71bbb8af0
gus <gustavohgrdev@gmaiil.com>
2025-02-11 18:08:55 -0300

    feat: implementa uma função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -11,5 +11,23 @@ function adicionarAmigo() {
     } else {
         amigos.push(input_value);
         input_value = "";
+        atualizarLista();
     }
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = "";
+
+    for (index = 0; index <= amigos.length; index++) {
+        let item = document.createElement("li");
+        item.textContent = amigos[index];
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    const random_number = (Math.floor(Math.random() * (amigos.length + amigos.length))) ;
+    
 }

1c1bef7a43a9ee3dbb3c05f6e2bc061310ad0042
Gustavo Henrique <145779028+hgrgustavo@users.noreply.github.com>
2025-02-12 19:54:27 -0300

    fix: defini melhor o regexp
    nomes com acentos agora são aceitos.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let amigos = [];
 
 function adicionarAmigo() {
     let input_value = document.querySelector("input").value.trim();
-    const regexp = /^[a-zA-Z\s]{3,30}$/;
+    const regexp = /^[a-zA-ZÀ-ÿ\s]{3,30}$/;
 
     if (!regexp.test(input_value)) {
         alert("Por favor, insira um nome.");

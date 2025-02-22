0e9340e0dced088c3ed9a268a7680ddb1d1dec39
newtsarthur <tutuhoficial13@gmail.com>
2025-02-22 12:47:26 -0300

    variáveis coletadas da página html  adicionadas ao script js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
 // Aqui você deverá desenvolver a lógica para resolver o problema.
+const inputAmigo = document.getElementById('amigo');
+const buttonAdd = document.getElementById('.button-add');
+const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
+const buttonDraw = document.getElementById('.button-draw');

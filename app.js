6bb1a6b7110183c227bfbba07cb7574d00144ea7
suellensilva86 <suellen.cunha.silva@gmail.com>
2025-03-17 11:53:10 -0300

    Add files via upload
    Inclusão do arquivo app.js desenvolvido por mim para o projeto.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+function adicionarAmigo (){
+    let amigo = document.querySelector("#amigo").value
+    amigos.push(amigo)
+    listarAmigos()
+}
+function listarAmigos(){
+    let lista = document.querySelector("#listaAmigos")
+    lista.textContent = ""
+    for (var amigo of amigos){
+        let item = document.createElement("li");
+        item.innerHTML = amigo;
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0) {
+        return
+    }
+
+    let pos = Math.floor(Math.random() * amigos.length)
+    document.querySelector('#resultado').innerText = amigos[pos]
+    delete amigos[pos]
+    amigos = amigos.filter(amigo => amigos)
+    listarAmigos()
+}
+
+listarAmigos()
